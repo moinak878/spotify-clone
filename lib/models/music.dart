ab4497d7f61ac040eec40b8d34f3cb834da78e7f
Moinak Banerjee <moinak878@gmail.com>
2022-06-23 18:37:11 +0530

    added music cards in home

---
+++ lib/models/music.dart
@@ -0,0 +1,7 @@
+class Music{
+  String name;
+  String imageURL;
+  String description;
+  String audioURL;
+  Music(this.name , this.imageURL, this.description,this.audioURL);
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:spotify/models/music.dart';
 import 'package:spotify/services/category_operations.dart';
-import '../models/category.dart';
+import 'package:spotify/services/music_operations.dart';
+import 'package:spotify/models/category.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -19,7 +21,7 @@ class Home extends StatelessWidget {
       greeting = "Good Evening";
     }
     return Padding(
-      padding: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.only(top: 10),
       child: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0.0, // by default some elevation present , shows a border
@@ -35,78 +37,151 @@ class Home extends StatelessWidget {
       ),
     );
   }
-  
+  Widget createCategory(Category category){
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        color: Colors.grey.withOpacity(0.3),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(5),
+        child: Row(
+          children: [
+            Image.network(category.imageURL , fit : BoxFit.cover),
+            Padding(
+              padding: EdgeInsets.only(left : 10),
+              child: Text(category.name , style: TextStyle(
+                  color : Colors.white ,
+                  fontWeight: FontWeight.bold
+              ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> createListofCategories(){
+    return CategoryOperations.getCategories()
+        .map((category) => createCategory(category))
+        .toList();
+  }
+
   Widget createGrid(){
-
-    Widget createCategory(Category category){
-      return Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-          color: Colors.grey.withOpacity(0.3),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(5),
-          child: Row(
-            children: [
-              Image.network(category.imageURL , fit : BoxFit.cover),
-              Padding(
-                padding: EdgeInsets.only(left : 10),
-                child: Text(category.name , style: TextStyle(
-                    color : Colors.white ,
-                    fontWeight: FontWeight.bold
-                ),
-                ),
-              )
-            ],
-          ),
-        ),
-      );
-    }
-
-    List<Widget> createListofCategories(){
-      return CategoryOperations.getCategories()
-          .map((category) => createCategory(category))
-          .toList();
-    }
-    
     return Container(
       padding: EdgeInsets.all(5),
-      height: 400,
+      height: 190,
       child: GridView.count(
         childAspectRatio : 5/2 ,// width / height
         crossAxisCount: 2,
-        crossAxisSpacing: 8,
-        mainAxisSpacing: 8,
+        crossAxisSpacing: 5,
+        mainAxisSpacing: 5,
         children : createListofCategories()
       ),
     );
   }
 
+  // Music Widget
+  Widget createMusic(Music music){
+    return Container(
+      padding: EdgeInsets.all(5),
+      margin: EdgeInsets.all(5),
+      height: 110,
+      decoration: BoxDecoration(
+        color: Colors.grey.withOpacity(0.1),
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      ),
+      child: Column(
+        crossAxisAlignment : CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            height: 110,
+            width: 110,
+            child: Image.network(
+              music.imageURL,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Text(
+            music.name ,
+            style: TextStyle(
+              color : Colors.white ,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            music.description,
+            style: TextStyle(
+              color: Colors.blueGrey.shade400,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget createMusicListview(String label){
+    List<Music> musicList = MusicOperations.getMusic();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children : [
+        Container(
+          padding: EdgeInsets.fromLTRB(10, 0, 0, 5),
+          child: Text(
+            label,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+        padding: EdgeInsets.all(10),
+        height: 230,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+            itemBuilder: (ctx,index){
+              return createMusic(musicList[index]);
+            },
+            itemCount: musicList.length
+        ),
+      ),
+        ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors : [
-                  Colors.blueGrey.shade300,
-                  Colors.black
-                ],
-                begin: Alignment.topLeft,
-                end : Alignment.bottomRight,
-                stops: const [0.1 , 0.3],
-              )
-          ),
-          child: Column(
-            children:[
-              createAppBar(),
-              SizedBox(
-                height : 30,
-              ),
-              createGrid(),
-            ],
-          ),
-        )
+    return SingleChildScrollView(
+      child: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors : [
+                    Colors.blueGrey.shade300,
+                    Colors.black
+                  ],
+                  begin: Alignment.topLeft,
+                  end : Alignment.bottomRight,
+                  stops: const [0.1 , 0.3],
+                )
+            ),
+            child: Column(
+              children:[
+                createAppBar(),
+                SizedBox(
+                  height : 30,
+                ),
+                createGrid(),
+                createMusicListview("Made for you"),
+                createMusicListview("Popular Playlists"),
+              ],
+            ),
+          )
+      ),
     );
   }
 }
