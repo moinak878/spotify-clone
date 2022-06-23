5591b4d4d63b6ccb5ba7ca084f82b33f885d7d66
Moinak Banerjee <moinak878@gmail.com>
2022-06-23 15:26:12 +0530

    added app bar in home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,20 +4,53 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
+  Widget createAppBar(){
+    String greeting;
+    final hour = DateTime.now().hour;
+    if(hour<=12){
+      greeting="Good Morning";
+    }
+    else if(hour<17){
+      greeting = "Good Afternoon";
+    }
+    else{
+      greeting = "Good Evening";
+    }
+    return Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0, // by default some elevation present , shows a border
+        title: Text( greeting , style: TextStyle(fontSize: 30)),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 20),
+              child: Icon(
+                  Icons.settings , color: Colors.white
+              )
+          )
+        ],
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Container(
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors : [
-                Colors.blueGrey.shade300,
-                Colors.black
-              ],
-              begin: Alignment.topLeft,
-              end : Alignment.bottomRight,
-              stops: const [0.1 , 0.3],
-            )
+              gradient: LinearGradient(
+                colors : [
+                  Colors.blueGrey.shade300,
+                  Colors.black
+                ],
+                begin: Alignment.topLeft,
+                end : Alignment.bottomRight,
+                stops: const [0.1 , 0.3],
+              )
+          ),
+          child: Column(
+            children:[
+              createAppBar()],
           ),
         )
     );
